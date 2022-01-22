8825741197cdb33e3c642d42c33ba67b2fce8895
Francisco Silva <fcaceres.silva1@gmail.com>
2022-01-22 14:24:40 +0000

    Done

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,7 +1,7 @@
 class Bookmark < ApplicationRecord
-  belongs_to :movie
   belongs_to :list
+  belongs_to :movie
 
-  validates :comment, length: { minimum: 6 }
-  validates_uniqueness_of :movie_id, scope: [:list_id]
+  validates :comment, length: { minimum: 6 }, presence: true
+  validates :movie_id, uniqueness: { scope: :list_id }
 end
